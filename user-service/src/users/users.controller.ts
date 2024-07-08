b141587e2fafb37c888b6f6335fdfa54dd7e89d7
Danil Markov <markov.danil.04@mail.ru>
2024-07-08 11:45:05 +0300

    settings

--- user-service/src/users/users.controller.ts
+++ user-service/src/users/users.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get('/')
+  async getAllUsers() {
+    return [
+      { id: 1, name: 'danil' },
+      { id: 2, name: 'Alen' },
+    ];
+  }
 }
